198f4bc05eb65f89b2f0cd4b80e7df3ff14eb4a8
junnuvain10 <97727062+junnuvain10@users.noreply.github.com>
2026-03-13 12:07:38 +0200

    Update file paths for bulk inserts in SQL script

--- scripts/bronze_layer/proc_load_bronze.sql
+++ scripts/bronze_layer/proc_load_bronze.sql
@@ -39,7 +39,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\Junnu\Desktop\Dataprojektit\data_warehouse_project\source_crm\cust_info.csv'
+		FROM 'C:\Users\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -56,7 +56,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info 
-		FROM 'C:\Users\Junnu\Desktop\Dataprojektit\data_warehouse_project\source_crm\prd_info.csv'
+		FROM 'C:\Users\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -73,7 +73,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\Junnu\Desktop\Dataprojektit\data_warehouse_project\source_crm\sales_details.csv'
+		FROM 'C:\Users\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -94,7 +94,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\Junnu\Desktop\Dataprojektit\data_warehouse_project\source_erp\cust_az12.csv'
+		FROM 'C:\Users\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -111,7 +111,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\Junnu\Desktop\Dataprojektit\data_warehouse_project\source_erp\loc_a101.csv'
+		FROM 'C:\Users\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -128,7 +128,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\Junnu\Desktop\Dataprojektit\data_warehouse_project\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\Users\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
